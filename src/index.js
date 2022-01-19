c0f559634ea87558cc560e20559714ac0afa21c3
cheney <cbed@163.com>
2022-01-19 18:06:07 +0800

    初步实现需求

---
+++ src/index.js
@@ -0,0 +1,195 @@
+/**
+ * 属性匹配检查
+ * @param rule
+ * @param attrs
+ * return true/false
+ */
+function match( rule , attrs) {
+    // rule 必须是字符串
+    if( ! _isString( rule ) ) {
+        throw 'rule 必须是字符串, rule=' + rule
+    }
+
+    // attrs 支持单层 map 或者 url
+    if ( _isString( attrs ) ) {
+        attrs = _getUrlParams( attrs )
+    }
+
+    // 将 rule 按照 , 切开, 逻辑或
+    let selectors = rule.split(",")
+    for ( let selector of selectors ) {
+        let item = selector.trim()
+        // 正则提取 attr opt value
+        let re = /^\[((?<attr1>\/.+\/)|(?<attr2>".+")|(?<attr3>'.+')|(?<attr4>[^\*\|\^\$\!=]+)?)((?<opt>[\*\|\^\$\!]?=)((?<value1>\/.+\/)|(?<value2>".+")|(?<value3>'.+')|(?<value4>.+)))?]$/
+        let m = item.match( re );
+        if ( !m || !m.groups ) {
+            console.error( "规则解析失败", m.groups )
+            return false
+        }
+
+        let { attr1, attr2, attr3, attr4, opt, value1, value2, value3, value4 } = m.groups
+        let attr = _toTypedValue( attr1, attr2 || attr3, attr4 )
+        let value = _toTypedValue( value1, value2 || value3, value4 )
+
+        // 属性是正则，要对所有属性进行遍历
+        if ( _isRegExp( attr ) ) {
+            if ( "undefined" === value ) {
+                // undefined 特殊处理
+                // 期望所有匹配的属性都不存在，有任何一个属性存在都算不匹配。
+                // 全部都是 false ，return true
+                // 任何一个 true ， return false
+                for ( let k in attrs ) {
+                    let v = attrs[k]
+                    if( attr.test( k ) &&                           // key 匹配
+                        _match(undefined, opt, v)  // v 有值
+                    ){                                              // ||
+                        return false                                // 逻辑不匹配
+                    }
+                }
+                return true
+            } else {
+                // 任何一个 true ， return true
+                for ( let k in attrs ) {
+                    let v = attr[k]
+                    if( _match( opt, value, v ) ){
+                        return true
+                    }
+                }
+                return false
+            }
+        } else {
+            // 属性是值
+            let v = attrs[ attr ]
+            if( _match( opt, value, v ) ){
+                return true
+            }
+            return false
+        }
+    }
+    return false
+}
+
+
+/**
+ *
+ * @param rule
+ * @param objs 要求是数组,内部的元素要求需要 id 属性
+ * @param pick
+ * return 被选中的对象数组
+ */
+function select( rule , objs , pick ) {
+    let array = []
+
+    // objs 要求是数组
+    if ( _isArray( objs ) ) {
+        throw 'objs 要求是数组, objs=' + objs
+    }
+
+    for ( let obj of objs ) {
+        // TODO rule 每次都会被解析一遍，能不能优化。
+        let ret  = match( rule, obj )
+        if ( ret ) {
+            // 默认返回 id, 如果有 pick 则返回 pick 的执行结果。
+            if ( pick && _isFunction( pick )) {
+                array.push(  pick( obj ) )
+            } else {
+                array.push( obj.id )
+            }
+        }
+    }
+    return array;
+}
+
+
+/**
+ *
+ * @param opt 操作符
+ * @param value 值表达式
+ * @param v 当前值
+ * @returns {boolean}
+ * @private
+ */
+function _match(opt, value, v){
+
+    switch ( opt ) {
+        case undefined : {
+            // 没有操作符，也没有 value, 只要 v 存在就行
+            return "undefined" !== v && undefined !== v
+        }
+        case "=" : {
+            if ( value === "undefined" ) {
+                return "undefined" === v || undefined === v
+            }
+            else if ( _isRegExp( value ) ) {
+                return value.test( v )
+            }
+            else {
+                return value === v
+            }
+        }
+        case "!=" : {
+            if ( value === "undefined" ) {
+                return "undefined" !== v && undefined !== v
+            }
+            else {
+                return value !== v
+            }
+        }
+        case "*=" : {
+            return v.indexOf(value) >= 0
+        }
+        case "|=" : {
+            return new RegExp( "^" + value + "\\W", "mg").test( v );
+        }
+        case "^=" : {
+            return new RegExp( "^" + value, "mg").test( v );
+        }
+        case "$=" : {
+            return new RegExp( value + "$", "mg").test( v );
+        }
+    }
+    return false;
+}
+
+function _toTypedValue(value1, value23, value4){
+    if ( value4 ) {
+        return value4
+    }
+    if ( value23 ) {
+        return value23.substr(1, value23.length -2 )
+    }
+    if ( value1 ) {
+        return new RegExp(value1.substr(1, value1.length -2 ), "mg");
+    }
+}
+
+
+function _isRegExp(value) {
+    return Object.prototype.toString.call(value) === '[object RegExp]'
+}
+
+function _isArray(value) {
+    return Object.prototype.toString.call(objs) !== '[object Array]'
+}
+
+function _isString(value) {
+    return typeof(value) === 'string'
+}
+
+function _isFunction(value) {
+    return typeof value === "function"
+}
+
+function _getUrlParams(url){
+    let params = url.split("#")[0].split("?")[1].split("&")
+    let obj = {}
+    params.map(function (item){
+        obj[ decodeURIComponent( item.split("=")[0]) ] = decodeURIComponent(item.split("=")[1])
+    })
+    return obj
+}
+
+
+module.exports = {
+    match , select
+}
---
+++ test/test.js
@@ -0,0 +1,197 @@
+const {expect, assert} = require('chai');
+const Selector = require('../src/index')
+
+
+describe('match 正向测试 包含某个属性 1',function(){
+    it('Selector 1-1 包含某个属性',function(){
+
+        assert( Selector.match("[test]", { test : "test" }) )
+
+    });
+
+    it('Selector 1-2 包含某个属性',function(){
+
+        assert( Selector.match("['test']", { test : "test" }) )
+
+    });
+
+    it('Selector 1-3 包含某个属性',function(){
+
+        assert( Selector.match("[\"test\"]", { test : "test" }) )
+
+    });
+
+    it('Selector 1-4 包含某个属性',function(){
+
+        assert( Selector.match("[/test/]", { test : "test" }) )
+
+    });
+
+});
+
+
+
+describe('match 异常测试 包含某个属性 2',function(){
+    it('Selector 2-1 包含某个属性',function(){
+
+        assert( ! Selector.match("[test]", { testx : "test" }) )
+
+    });
+
+    it('Selector 2-2 包含某个属性',function(){
+
+        assert( ! Selector.match("['test']", { testx : "test" }) )
+
+    });
+
+    it('Selector 2-3 包含某个属性',function(){
+
+        assert( ! Selector.match("[\"test\"]", { testx : "test" }) )
+
+    });
+
+    it('Selector 2-4 包含某个属性',function(){
+
+        assert( ! Selector.match("[/test/]", { testx : "test" }) )
+
+    });
+
+});
+
+
+
+describe('match 异常测试 不包含某个属性 3',function(){
+    it('Selector 3-1 不包含某个属性',function(){
+
+        assert( Selector.match("[test=undefined]", { testx : "test" }) )
+
+    });
+
+    it('Selector 3-2 不包含某个属性',function(){
+
+        assert( Selector.match("['test'=undefined]", { testx : "test" }) )
+
+    });
+
+    it('Selector 3-3 不包含某个属性',function(){
+
+        assert( Selector.match("[\"test\"=undefined]", { testx : "test" }) )
+
+    });
+
+    it('Selector 3-4 不包含某个属性',function(){
+
+        assert( Selector.match("[/test/=undefined]", {}) )
+
+    });
+
+});
+
+describe('match 异常测试 不包含某个属性 4',function(){
+    it('Selector 4-1 不包含某个属性',function(){
+
+        assert( ! Selector.match("[test=undefined]", { test : "test" }) )
+
+    });
+
+    it('Selector 4-2 不包含某个属性',function(){
+
+        assert( ! Selector.match("['test=undefined']", { test : "test" }) )
+
+    });
+
+    it('Selector 4-3 不包含某个属性',function(){
+
+        assert( ! Selector.match("[\"test\"=undefined]", { test : "test" }) )
+
+    });
+
+    it('Selector 4-4 不包含某个属性',function(){
+
+        assert( ! Selector.match("[/test/=undefined]", { test : "test" }) )
+
+    });
+
+});
+
+
+describe('match 匹配 5',function(){
+    it('Selector 5-1 *=',function(){
+
+        assert( Selector.match("[test*=t]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-2 *=',function(){
+
+        assert( ! Selector.match("[test*=a]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-3 |=',function(){
+
+        assert( Selector.match("[test|=t]", { test : "t-test" }) )
+
+    });
+
+    it('Selector 5-4 |=',function(){
+
+        assert( ! Selector.match("[test|=t]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-5 ^=',function(){
+
+        assert( Selector.match("[test^=t]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-6 ^=',function(){
+
+        assert( ! Selector.match("[test^=a]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-7 $=',function(){
+
+        assert( Selector.match("[test$=t]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-8 $=',function(){
+
+        assert( ! Selector.match("[test$=a]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-9 $=',function(){
+
+        assert( ! Selector.match("[test$=t]", { testx : "test" }) )
+
+    });
+
+    it('Selector 5-10 $=',function(){
+
+        assert( ! Selector.match("[test$=a]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-11 !=',function(){
+
+        assert( Selector.match("[test!=t]", { test : "test" }) )
+
+    });
+
+    it('Selector 5-12 !=',function(){
+
+        assert( Selector.match("[test!=test]", { testx : "test" }) )
+
+    });
+
+    it('Selector 5-13 !=',function(){
+
+        assert( ! Selector.match("[test!=test]", { test : "test" }) )
+
+    });
+
+});
